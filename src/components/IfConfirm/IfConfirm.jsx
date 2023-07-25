9fa22a972843079387c8b451b45e0df6ec6c4e42
iFlameing <ialokkumarsingh0@gmail.com>
2023-07-25 18:50:00 +0530

    Change ifconfirm component for compatibility with volto-light-theme

--- src/components/IfConfirm/IfConfirm.jsx
+++ src/components/IfConfirm/IfConfirm.jsx
@@ -12,7 +12,7 @@ const IfConfirm = ({ children, module, showDSGVOBanner }) => {
   // a bug in the caller, there is nothing we can do here,
   // but we want to avoid a traceback.
   // See https://gitlab.dlr.de/internet-cms/cms-plone/dlr-internet/-/issues/1384
-  if (!module || !!Number(cookies[`confirm_${module?.cookie}`])) {
+  if (!module || !!Number(cookies[`confirm_${module}`])) {
     return <>{children}</>;
   } else {
     return (
