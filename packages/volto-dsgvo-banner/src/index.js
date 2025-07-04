f479714dcb5485f10c089dd1130fdadc99244dbb
Tishasoumya-02 <tishasoumya@gmail.com>
2025-07-04 20:15:26 +0530

    fix:lint

--- packages/volto-dsgvo-banner/src/index.js
+++ packages/volto-dsgvo-banner/src/index.js
@@ -28,7 +28,7 @@ const applyConfig = (config) => {
     },
     modules: ['tracking', 'youtube', 'facebook', 'google'],
     showBanner: false,
-    showOverlay:true,
+    showOverlay: true,
     showTechnicallyRequired: true,
     cssClasses: {
       bannerAgreeButton: 'branded olive',
@@ -37,11 +37,11 @@ const applyConfig = (config) => {
     privacy_url: '/privacy',
     ...(config.settings.DSGVOBanner || {}),
   };
-  config.settings.loadables['reactGa'] = loadable.lib(() =>
-    import('react-ga4'),
+  config.settings.loadables['reactGa'] = loadable.lib(
+    () => import('react-ga4'),
   );
-  config.settings.loadables['matomoTracker'] = loadable.lib(() =>
-    import('@datapunt/matomo-tracker-js'),
+  config.settings.loadables['matomoTracker'] = loadable.lib(
+    () => import('@datapunt/matomo-tracker-js'),
   );
   config.blocks.blocksConfig.dsgvoBanner = {
     id: 'dsgvoBanner',
