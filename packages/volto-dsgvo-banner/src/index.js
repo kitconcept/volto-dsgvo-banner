2691f03ccdd2a17ad68a1d8d41c15e7200a73dad
iFlameing <ialokkumarsingh0@gmail.com>
2026-03-25 20:37:15 +0530

    remove ci-trigger commit

--- packages/volto-dsgvo-banner/src/index.js
+++ packages/volto-dsgvo-banner/src/index.js
@@ -32,7 +32,6 @@ const applyConfig = (config) => {
     // to accept cookies until they see a block where they are needed.
     showBanner: true,
     showTechnicallyRequired: true,
-    ci-trigger:true,
     cssClasses: {
       bannerAgreeButton: 'branded olive',
       bannerAdjustButton: 'branded blue',
